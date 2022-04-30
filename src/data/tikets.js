b689ad729ecd3d2c4397675638044813de5b8e70
Cleiton Marques <cleiton.p.marques@gmail.com>
2022-04-30 15:52:34 +0100

    Data and list ticket

---
+++ src/data/tikets.js
@@ -0,0 +1,21 @@
+export default
+    [
+        {
+            id: 1,
+            title: "Meu primeiro ticket",
+            content: "Este é um exemplo de ticket aberto",
+            currentStatus: "Aberto",
+            openingDate: "30/04/2022 8:00",
+            lastUpdate: "30/04/2022 14:00",
+            lastOperatorUpdated: "Cleiton Marques"
+        },
+        {
+            id: 2,
+            title: "Feito em Svelte",
+            content:"Ticket criado no sistema de chamados HiDesk em Svelte",
+            currentStatus: "Aberto",
+            openingDate: "30/04/2022 8:00",
+            lastUpdate: "30/04/2022 14:00",
+            lastOperatorUpdated: "Cleiton Marques"
+        }
+    ];
